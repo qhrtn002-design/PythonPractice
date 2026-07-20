625751950bd17fdc5ada737a6ee7bd373406451d
qhrtn002-design <qhrtn002@gmail.com>
2026-07-20 22:52:29 +0900

    두개의 숫자열

--- SWEA/D2/1959. 두 개의 숫자열.py
+++ SWEA/D2/1959. 두 개의 숫자열.py
@@ -3,13 +3,20 @@ t = int(input())
 for s in range(1, t+1):
     n, m = map(int, input().split())
 
-    lst_a = list(map(int, input().split()))
-    lst_b = list(map(int, input().split()))
+    lst_a = list(map(int, input().split())) #리스트 받기
+    lst_b = list(map(int, input().split())) #리스트 받기
 
-    total = 0
-    for x in range(len(lst_a)):
-        for y in range(len(lst_b)):
-            total += lst_a[x] * lst_b[y]
+    maxans = 0 #합계 최댓값
+    for ct in range(abs(n-m)+1): #리스트 간격으로 반복횟수 잡기
+        total = 0 #순간 3개 곱의 합계
+        for x in range(min(n,m)): #두 리스트 중 짧은 길이만큼만 반복
+            if n > m: #리스트 중 하나가 길면,
+                total += lst_a[x+ct] * lst_b[x]
+                #긴 쪽의 리스트가 하나씩 움직이며 다시 대조
+            else:
+                total += lst_a[x] * lst_b[x+ct]
+        
+        if total > maxans: #3개 곱의 합이 최댓값보다 크면
+            maxans = total #최댓값 바꾸기
 
-
-    print(f'#{s} {total}')
+    print(f'#{s} {maxans}')
