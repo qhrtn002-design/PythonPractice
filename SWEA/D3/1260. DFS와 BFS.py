743a23d0de7cb443ade3305ee058df8b062c3804
qhrtn002-design <qhrtn002@gmail.com>
2026-08-06 02:45:27 +0900

    1

---
+++ SWEA/D3/1260. DFS와 BFS.py
@@ -0,0 +1,3 @@
+n, m, v = map(int, input().split())
+for s in range(m):
+    a, b = map(int, input().split())
--- SWEA/D3/1873. 상호의 배틀필드.py
+++ SWEA/D3/1873. 상호의 배틀필드.py
@@ -14,35 +14,35 @@ for s in range(t):
             if arr[i][j] in tank:
                 tankpoint.append(i)
                 tankpoint.append(j)
-    print(tankpoint)
-
+    
     for m in move:
-        for k in range(4):
-            if m == cmd[k]:
-                arr[tankpoint[0]][tankpoint[1]] = '.'
-                nx = tankpoint[0] + dx[k]
-                ny = tankpoint[1] + dy[k]                
-                if 0 <= nx <h and 0 <= ny < w and arr[nx][ny] == '.':
-                    arr[nx][ny] = tank[k]
-                else:
-                    arr[tankpoint[0]][tankpoint[1]] = tank[k]
+        if m in cmd:
+            k = cmd.index(m)
+            arr[tankpoint[0]][tankpoint[1]] = '.'
+            nx = tankpoint[0] + dx[k]
+            ny = tankpoint[1] + dy[k]                
+            if 0 <= nx <h and 0 <= ny < w and arr[nx][ny] == '.':
+                arr[nx][ny] = tank[k]
+                tankpoint[0] = nx 
+                tankpoint[1] = ny 
+            else:
+                arr[tankpoint[0]][tankpoint[1]] = tank[k]
 
-            else: #인덱스 k 를 써야되는데 못쓰고 있다
-                bx = tankpoint[0] + dx[k]
-                by = tankpoint[1] + dy[k]
-                if 0 <= bx < h and 0 <= by < w and arr[bx][by] == '.' or arr[bx][by] == '-':
-                    if arr[bx][by] == '*':
-                        arr[bx][by] = '.'
-            
-    print(f'#{s+1}',end='')
+        else:
+            if arr[tankpoint[0]][tankpoint[1]] in tank:
+                p = tank.index(arr[tankpoint[0]][tankpoint[1]])
+                boltx = tankpoint[0] + dx[p]
+                bolty = tankpoint[1] + dy[p]
+                while 0<=boltx < h and 0<=bolty < w:
+                    if arr[boltx][bolty] == '-' or arr[boltx][bolty] == '.':
+                        boltx += dx[p]
+                        bolty += dy[p]
+                    elif arr[boltx][bolty] == '*':
+                        arr[boltx][bolty] = '.'
+                        break
+                    elif arr[boltx][bolty] == '#':
+                        break
+             
+    print(f'#{s+1} ',end='')
     for row in arr:
-        print(*row)
-# 문자	의미
-# .	평지(전차가 들어갈 수 있다.)
-# *	벽돌로 만들어진 벽
-# #	강철로 만들어진 벽
-# -	물(전차는 들어갈 수 없다.)
-# ^	위쪽을 바라보는 전차(아래는 평지이다.)
-# v	아래쪽을 바라보는 전차(아래는 평지이다.)
-# <	왼쪽을 바라보는 전차(아래는 평지이다.)
-# >	오른쪽을 바라보는 전차(아래는 평지이다.)
+        print(''.join(row))
