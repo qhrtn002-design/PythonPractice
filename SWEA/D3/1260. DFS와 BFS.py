94d0eb38c3fd146f626a068f11229f3928181266
heesu seo <heesu710@naver.com>
2026-08-06 17:53:52 +0900

    ssafy

--- SWEA/D3/1260. DFS와 BFS.py
+++ SWEA/D3/1260. DFS와 BFS.py
@@ -1,3 +1,4 @@
 n, m, v = map(int, input().split())
 for s in range(m):
     a, b = map(int, input().split())
+    
---
+++ SWEA/D3/4831. 전기버스.py
@@ -0,0 +1,7 @@
+t = int(input())
+for s in range(t):
+    k,n,m = map(int, input().split())
+    charge = list(map(int,input().split()))
+    
+    
+    print(f'#{s+1} {chargecnt}')
